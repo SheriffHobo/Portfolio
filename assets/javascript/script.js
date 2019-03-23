5bd84ab07a44f3abb5b3ea7edc4b9e08c47b2290
SheriffHobo <zombiemaster@gmail.com>
2019-03-23 10:18:17 -0700

    flex fix on main

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,24 @@
-$(function() {
+$(function () {
 
-    $('#portfolio').on('click', function(event) {        
-        $('#main').toggle('show');
-   });
-      
+    $('#portfolio').on('click', function (event) {
+        if ($('#contact').val('style' === "display") ){
+            $('#contact').val('style', "display: none;")
+        } else {
+            $('#main').toggle('show');
+        }
+    });
+
+    $('#contact').on('click', function (event) {
+        if ($('#portfolio').val('style' === "display") ){
+            $('#portfolio').val('style', "display: none;")
+        } else {
+            $('#main').toggle('show');
+        }
+    });
+
+    $('[data-toggle="popover"]').popover({
+        placement: 'top',
+        trigger: 'hover'
+    });
+
 });
